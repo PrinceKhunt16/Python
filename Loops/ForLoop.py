081fce08a904a63b23b7ecaa71e258d9461837d5
PrinceKhunt16 <princekhuntdatastructure@gmail.com>
2022-12-14 22:42:38 +0530

    For Loop

---
+++ Loops/ForLoop.py
@@ -0,0 +1,20 @@
+name = 'john'
+
+for i in name: 
+  print(i)
+
+colors = ['Red', 'Green', 'Blue', 'Yellow']
+
+for color in colors:
+  print(color)
+  for i in color:
+    print(i)
+
+for k in range(10):
+  print(k)
+
+for k in range(1, 9):
+  print(k)
+
+for k in range(1, 10, 2):
+  print(k)
