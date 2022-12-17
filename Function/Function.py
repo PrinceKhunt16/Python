cb7561600ba6e3729649907ed1bdad809a7872d1
PrinceKhunt16 <princekhuntdatastructure@gmail.com>
2022-12-17 21:31:52 +0530

    Function

---
+++ Function/Function.py
@@ -0,0 +1,22 @@
+# Example 1
+
+# def sumOfTwoNumber(a, b):
+#   mean = int(a) + int(b)
+#   return mean
+
+# a = input('Enter value of a ')
+# b = input('Enter value of b ')
+
+# sum = sumOfTwoNumber(a, b)
+# print('Sum of a and b is', sum)
+
+# Example 2
+
+def concatName(name, surename):
+  return name + ' ' + surename
+
+name = 'John'
+surename = 'Doe'
+
+fullName = concatName(name, surename)
+print('Your full name is', fullName)
