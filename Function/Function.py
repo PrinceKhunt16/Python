3dd2192b6a0fc804775c59531f8a96f95110c09f
PrinceKhunt16 <princekhuntdatastructure@gmail.com>
2022-12-18 21:08:09 +0530

    Function difference type of arguments

--- Function/Function.py
+++ Function/Function.py
@@ -1,4 +1,4 @@
-# Example 1
+### Example 1
 
 # def sumOfTwoNumber(a, b):
 #   mean = int(a) + int(b)
@@ -10,13 +10,52 @@
 # sum = sumOfTwoNumber(a, b)
 # print('Sum of a and b is', sum)
 
-# Example 2
+### Example 2
 
-def concatName(name, surename):
-  return name + ' ' + surename
+# def concatName(name, surename):
+#   return name + ' ' + surename
 
-name = 'John'
-surename = 'Doe'
+# name = 'John'
+# surename = 'Doe'
 
-fullName = concatName(name, surename)
-print('Your full name is', fullName)
+# fullName = concatName(name, surename)
+# print('Your full name is', fullName)
+
+### Example 3
+
+# def average(a = 0, b = 0):
+#   print('The average is', (a + b) / 2)
+
+# average()
+
+### Example 4
+
+# def average(a, b):
+#   print('The average is', (a + b) / 2)
+
+# average(10, 20)
+
+### Example 5
+
+# def average(a, b):
+#   print('The average is', (a + b) / 2)
+
+# average(b = 20, a = 10)
+
+### Example 6
+
+# def average(*nums):
+#   num = 0
+#   for n in nums:
+#     num += n
+#   return num / len(nums)
+
+# c = average(1, 2, 3, 4, 5)
+# print(c)
+
+### Example 7
+
+# def name(**name):
+#   print('Hello,', name['fname'], name['mname'], name['lname'])
+
+# name(mname = 'Buchanan', lname = 'Barnes', fname = 'James')
