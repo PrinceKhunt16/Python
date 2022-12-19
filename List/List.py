e99a6ddaeecf9710cc40873c0489f9c4472f1a2e
PrinceKhunt16 <princekhuntdatastructure@gmail.com>
2022-12-19 20:42:21 +0530

    List

---
+++ List/List.py
@@ -0,0 +1,27 @@
+marks = [53, 85, 67, 'John', True]
+
+# print(marks)
+# print(marks[0])
+# print(marks[4])
+# print(marks)
+# print(marks[-3])
+# print(marks[-2])
+
+
+# if 67 in marks:
+#   print('Yes')
+# else:
+#   print('No')
+
+
+# print(marks[:])
+# print(marks[1:-1])
+# print(marks[1:4])
+# print(marks[0:4:2])
+
+
+# nums = [i for i in range(9)]
+# print(nums)
+
+# nums = [i*i for i in range(9) if i % 2 == 0]
+# print(nums)
