223b6a2a3a65fc8792f792a7175d5720aed1f11d
PrinceKhunt16 <princekhuntdatastructure@gmail.com>
2022-12-20 22:09:25 +0530

    List methods

---
+++ List/ListMethods.py
@@ -0,0 +1,36 @@
+nums = [1, 9, 4, 6, 4]
+
+# print(nums)
+# nums.append(8)
+# print(nums)
+
+# nums.sort()
+# print(nums)
+# nums.reverse()
+# print(nums)
+
+# print(nums.index(4))
+
+# print(nums.count(4))
+
+# numms = nums
+# numms[0] = 0
+# print(nums)
+
+# numms = nums.copy()
+# numms[0] = 0
+# print(nums)
+# print(numms)
+
+# nums.insert(1, 7)
+# print(nums)
+
+# numms = [89, 23, 43]
+# nums.extend(numms)
+# print(nums)
+
+# numms = [89, 23, 43]
+# nummms = nums + numms
+# print(nums)
+# print(numms)
+# print(nummms)
