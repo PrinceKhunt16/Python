70d4eba724fc608d7665111890213a5b24989c85
PrinceKhunt16 <princekhuntdatastructure@gmail.com>
2022-12-22 08:07:49 +0530

    Tuple

---
+++ Tuple/Tuple.py
@@ -0,0 +1,14 @@
+tup = (1, 3, 5, 'Green', 'Mengo')
+
+# print(type(tup), tup)
+# print(tup[0])
+# print(tup[2])
+# print(tup[-1])
+# print(tup[-2])
+
+# if 3 in tup:
+#   print('Yes')
+
+# tupp = tup[1:3]
+# print(tup)
+# print(tupp)
