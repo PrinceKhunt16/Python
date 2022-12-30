53ac47e0e0bb8f901e83329343a306e747fbb589
PrinceKhunt16 <princekhuntdatastructure@gmail.com>
2022-12-30 09:28:06 +0530

    Set methods

---
+++ Set/SetMethods.py
@@ -0,0 +1,53 @@
+s1 = {1, 2, 3, 4}
+s2 = {3, 4, 5, 6}
+
+# print(s1.union(s2))
+
+# print(s1.intersection(s2))
+# s1.intersection_update(s2)
+# print(s1)
+
+# s1.update(s2)
+# print(s1, s2)
+
+# print(s1.symmetric_difference(s2))
+
+# print(s1.isdisjoint(s2))
+# s1 = {1, 2}
+# print(s1.isdisjoint(s2))
+
+# print(s1.issuperset(s2))
+# s1 = {1, 2, 3, 4, 5, 6}
+# print(s1.issuperset(s2))
+
+# print(s1.issubset(s2))
+# s1 = {3, 4, 5, 6}
+# print(s1.issubset(s2))
+
+# s1.add(5)
+# print(s1)
+
+# s1.remove(4)
+# print(s1)
+
+# s1.discard(4)
+# s1.discard(5)
+# print(s1)
+
+# e = s1.pop()
+# print(e)
+# print(s1)
+
+# del s1
+# print(s1)
+
+# s1.clear()
+# print(s1)
+
+# if 4 in s1:
+#   print('Yes')
+# else:
+#   print('No')
+
+# for i in s1:
+#   print(i)
