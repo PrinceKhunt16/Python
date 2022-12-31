83028fea6bcff10c174c819d2be2afec82f9eaba
PrinceKhunt16 <princekhuntdatastructure@gmail.com>
2022-12-31 20:46:13 +0530

    DIctionary methods

---
+++ Dictionary/DictionaryMethods.py
@@ -0,0 +1,33 @@
+emp1 = {
+  11: 45,
+  12: 90,
+  13: 48
+}
+
+emp2 = {
+  21: 78,
+  22: 40,
+  23: 69
+}
+
+# emp1.update(emp2)
+# print(emp1)
+
+# emp1.clear()
+# print(emp1)
+
+# emp = {}
+# emp.update(emp1)
+# print(emp)
+
+# emp1.pop(11)
+# print(emp1)
+
+# emp1.popitem()
+# print(emp1)
+
+# del emp1
+# print(emp1)
+
+# del emp1[11]
+# print(emp1)
