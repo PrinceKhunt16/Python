84eea24bb0fcc3297b044a7adce9ab1ef8147a79
PrinceKhunt16 <princekhuntdatastructure@gmail.com>
2023-01-16 22:53:16 +0530

    File io

---
+++ FileIO/FileIO.py
@@ -0,0 +1,22 @@
+# Read File
+
+# f = open('FileIO/Fun.txt', 'r')
+# text = f.read()
+# print(text)
+# f.close()
+
+# f = open('FileIO/Fun.txt', 'r')
+# while True:
+#   line = f.readline()
+#   if not line:
+#     break
+#   print(line)
+
+# Write File
+
+# f = open('FileIO/Fun.txt', 'a')
+# f.write('Fun ')
+# f.close()
+
+# with open('FileIO/Fun.txt', 'a') as f:
+#   f.write('Fun ')
