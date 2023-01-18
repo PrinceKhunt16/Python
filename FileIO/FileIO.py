5a45e1a50a00f12de110904f72778b662274be70
PrinceKhunt16 <princekhuntdatastructure@gmail.com>
2023-01-18 23:31:43 +0530

    Lambda function

--- FileIO/FileIO.py
+++ FileIO/FileIO.py
@@ -20,3 +20,24 @@
 
 # with open('FileIO/Fun.txt', 'a') as f:
 #   f.write('Fun ')
+
+# f = open('FileIO/Fun.txt', 'r')
+# f.seek(5)
+
+# data = f.read()
+# print(data)
+
+# f = open('FileIO/Fun.txt', 'r')
+# f.seek(5)
+
+# print(f.tell())
+
+# data = f.read()
+# print(data)
+
+# with open('FileIO/Fun.txt', 'w') as f:
+#   f.write('Fun Fun')
+#   f.truncate(3)
+
+# with open('FileIO/Fun.txt', 'r') as f:
+#   print(f.read())
---
+++ Function/LambdaFunction.py
@@ -0,0 +1,19 @@
+# def double(x):
+#   return x * 2
+
+### Example 1
+
+# double = lambda x: x * 2
+# print(double(5))
+
+### Example 2
+
+# avg = lambda x, y: (x + y) / 2
+# print(avg(5, 3))
+
+### Example 3
+
+# def apl(fun, value):
+#   return 10 + fun(value)
+
+# print(apl(lambda x: x * x * x, 10))
