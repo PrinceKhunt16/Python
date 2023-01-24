e3c2471a1b0ec185b866c856b58ab7db065466d7
PrinceKhunt16 <princekhuntdatastructure@gmail.com>
2023-01-24 14:04:13 +0530

    Class

---
+++ Oops/Class.py
@@ -0,0 +1,27 @@
+class Person:
+  name = 'Harry'
+  occupation = 'Software Developer'
+  networth = 10
+
+  def info(self):
+    print(f'{self.name} is a {self.occupation}')
+
+p = Person()
+
+# p.name = 'Shubham'
+# p.occupation = 'Accountant'
+ 
+# print(p.name)
+# print(p.occupation)
+
+
+# p.info()
+
+
+# b = Person()
+
+# p.name = 'Shubham'
+# p.occupation = 'Accountant'
+
+# b.info()
+# p.info()
